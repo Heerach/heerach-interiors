52683f95d74e3dcc880ad759ba05c81d2846a013
Heerach <heerachinteriors@gmail.com>
2025-04-25 22:52:58 +0530

    🚀 Deploy Heerach Interiors with admin upload + Google Auth

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyClBA1_h7NiOhK6a3uh4gSuUGZmbCm1iCA",
   authDomain: "heerach-495f1.firebaseapp.com",
   projectId: "heerach-495f1",
-  storageBucket: "heerach-495f1.appspot.com",
+  storageBucket: "heerach-495f1.firebasestorage.app",
   messagingSenderId: "933639217329",
   appId: "1:933639217329:web:b8b17fded3fc7ca8238290",
   measurementId: "G-M8TTNGJNFB"
@@ -59,7 +59,6 @@ const styleImages = {
 };
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(false);
   const [selectedStyle, setSelectedStyle] = useState(null);
   const [user, setUser] = useState(null);
   const [uploading, setUploading] = useState(false);
